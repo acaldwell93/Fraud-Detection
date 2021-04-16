6895ca8d71642f9eb09cb6c6f724ef0ecf7317e5
kyle-black <kpblack87@gmail.com>
2021-04-16 10:04:07 -0500

    flask2

--- Flask/app.py
+++ Flask/app.py
@@ -21,50 +21,23 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/eda')
-def eda():
 
-    column_options =[1,2,3,4]
-    return render_template("eda.html", column_options= column_options)
-
-@app.route('/graphs', methods=['POST'])
-def graphs():
-    col1 = int(request.form['column1']) - 1
-    col2 = int(request.form['column2']) - 1
-
-    if col1 == col2:
-        return f'Why do you want to graph column {col1+1} by itself?!'
-    else:
-        return render_template('graphs.html', col1=col1+1, col2=col2+1, url=f'./static/images/col{col1}col{col2}.png')  
-   
 @app.route('/predict')
 def predict():
+    data1= request.form['a']
+
+
+    arr = np.array([data1])
+
+    pred =model.predict(arr)
+    return render_template('results.html', data=pred)
+    
+    
+    
     return render_template('predict.html')
 
 
-'''
-@app.route('/results', methods=['POST'])
-def results():
-    sepal_length = float(request.form['sepal_length'])
-    sepal_width = float(request.form['sepal_width'])
-    petal_length = float(request.form['petal_length'])
-    petal_width = float(request.form['petal_width'])
 
-    test_data = np.array([sepal_length, sepal_width, petal_length, petal_width]).reshape(1, -1) 
-    filename = 'iris_log_regr.pkl'
-    with open(filename, 'rb') as f:
-    model = pickle.load(f)
-
-    prediction = model.predict(test_data)
-
-    data = load_iris()
-    target_names = data.target_names
-    for name in target_names[prediction]:
-        predicted_name = name
-    
-    return render_template('results.html', prediction=predicted_name, image=f'./static/images/iris.png')  
-   
-'''
 
 if __name__=="__main__":
     app.run(debug=True)
